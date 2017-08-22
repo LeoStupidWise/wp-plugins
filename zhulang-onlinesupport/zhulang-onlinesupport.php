650fb97f6dfcadadf62f8de33f56387ab2b6182e
YaZhou <stupidwise.leo@gmail.com>
2017-08-22 18:52:24 +0800

    分隔符添加多个联系方法（未进行信息校验，未使用JS添加多个联系方式，未进行提示美化），文章管理初步。

--- zhulang-onlinesupport/zhulang-onlinesupport.php
+++ zhulang-onlinesupport/zhulang-onlinesupport.php
@@ -21,11 +21,13 @@ class ZhulangOnlineSupport
     private $author               =  'zoe';
     private $menu_page_slug_top =  '';
     private $option_qq_number_1 =  '';
+    private $option_title        =  '';
     private $option_qq_number_2 =  '';
     private $option_qq_number_3 =  '';
     private $option_qq_number_4 =  '';
     private $option_phone        =  '';
     private $option_support_time=  '';
+    private $fengefu              =  ',';
 
 
     public function __construct()
@@ -36,10 +38,11 @@ class ZhulangOnlineSupport
         $this->menu_page_slug_top  =  $this->author.'_'.$this->name_en.'_page_menu_top';
 //        $this->option_name_html    = $this->option_prefix . '_html';
         //TODO 其实可以把 QQ 号码存在一条记录，用 JSON 保存，输入/输出做相关格式处理，暂时不搞那么先进
+        $this->option_title         = $this->option_prefix.'_title';
         $this->option_qq_number_1  = $this->option_prefix.'_qq_number_1';
-        $this->option_qq_number_2  = $this->option_prefix.'_qq_number_2';
-        $this->option_qq_number_3  = $this->option_prefix.'_qq_number_3';
-        $this->option_qq_number_4  = $this->option_prefix.'_qq_number_4';
+//        $this->option_qq_number_2  = $this->option_prefix.'_qq_number_2';
+//        $this->option_qq_number_3  = $this->option_prefix.'_qq_number_3';
+//        $this->option_qq_number_4  = $this->option_prefix.'_qq_number_4';
         $this->option_phone         = $this->option_prefix.'_phone';
         $this->option_support_time = $this->option_prefix.'_support_time';
     }
@@ -69,14 +72,16 @@ class ZhulangOnlineSupport
         // 简单形式：http://wpa.qq.com/msgrd?v=3&uin=14789544&site=http://www.ijoyer.com/&menu=yes
 
         // 窗口模式：<img  style="CURSOR: pointer" onclick="javascript:window.open('http://b.qq.com/webc.htm?new=0&sid=999999&o=test.yz.com&q=7', '_blank', 'height=502, width=644,toolbar=no,scrollbars=no,menubar=no,status=no');"  border="0" SRC=http://wpa.qq.com/pa?p=1:999999:1 alt="点击这里给我发消息">
+        $title          =  get_option($this->option_title);
         $phone          =  get_option($this->option_phone);
-        $support_time   =  get_option($this->option_support_time);
+//        $support_time   =  get_option($this->option_support_time);
         $slogan         =  '我们竭诚为您服务！';
         $qq_number_1    =  get_option($this->option_qq_number_1) ? get_option($this->option_qq_number_1) : $slogan;
-        $qq_number_2    =  get_option($this->option_qq_number_2) ? get_option($this->option_qq_number_2) : $slogan;
-        $qq_number_3    =  get_option($this->option_qq_number_3) ? get_option($this->option_qq_number_3) : $slogan;
-        $qq_number_4    =  get_option($this->option_qq_number_4) ? get_option($this->option_qq_number_4) : $slogan;
-        
+//        $qq_number_2    =  get_option($this->option_qq_number_2) ? get_option($this->option_qq_number_2) : $slogan;
+//        $qq_number_3    =  get_option($this->option_qq_number_3) ? get_option($this->option_qq_number_3) : $slogan;
+//        $qq_number_4    =  get_option($this->option_qq_number_4) ? get_option($this->option_qq_number_4) : $slogan;
+        $qq_numbers     =  explode($this->fengefu, $qq_number_1);
+        $phones         =  explode($this->fengefu, $phone);
         echo '<div id="goto_top"></div>';
         echo '<div id="online_qq_layer" show="1" style="right: 0px; display: block;">';
         echo '<div id="online_qq_tab">';
@@ -89,14 +94,22 @@ class ZhulangOnlineSupport
         echo '<div class="online_w_top"> </div>';
         echo '<div class="online_w_c overz">';
         echo '<div class="online_bar collapse" id="onlineSort1">';
-        echo '<h2> <a href="javascript:;">在线技术支持</a> </h2>';
+        echo "<h2> <a href='javascript:;'>$title</a> </h2>";
         echo '<div class="online_content overz" id="onlineType1" style="display: block;">';
         echo '<ul class="overz">';
-        echo "<li><a target='_blank' href='http://wpa.qq.com/msgrd?v=3&uin=$qq_number_1&site=qq&menu=yes'><img border='0' src='http://wpa.qq.com/pa?p=2:12345678:51' alt=$qq_number_1 title=$slogan></a></li>";
-        echo "<li><a target='_blank' href='http://wpa.qq.com/msgrd?v=3&uin=$qq_number_2&site=qq&menu=yes'><img border='0' src='http://wpa.qq.com/pa?p=2:12345678:51' alt=$qq_number_2 title=$slogan></a></li>";
-        echo "<li><a target='_blank' href='http://wpa.qq.com/msgrd?v=3&uin=$qq_number_3&site=qq&menu=yes'><img border='0' src='http://wpa.qq.com/pa?p=2:12345678:51' alt=$qq_number_3 title=$slogan></a></li>";
-        echo "<li><a target='_blank' href='http://wpa.qq.com/msgrd?v=3&uin=$qq_number_4&site=qq&menu=yes'><img border='0' src='http://wpa.qq.com/pa?p=2:12345678:51' alt=$qq_number_4 title=$slogan></a></li>";
-        echo "<li>电话：$phone</li>";
+        if (count($qq_numbers) > 0) {
+            foreach($qq_numbers as $key=>$qq_number) {
+                echo "<li><a target='_blank' href='http://wpa.qq.com/msgrd?v=3&uin=$qq_number&site=qq&menu=yes'><img border='0' src='http://wpa.qq.com/pa?p=2:12345678:51' alt=$qq_number title=$slogan></a></li>";
+            }
+        }
+//        echo "<li><a target='_blank' href='http://wpa.qq.com/msgrd?v=3&uin=$qq_number_2&site=qq&menu=yes'><img border='0' src='http://wpa.qq.com/pa?p=2:12345678:51' alt=$qq_number_2 title=$slogan></a></li>";
+//        echo "<li><a target='_blank' href='http://wpa.qq.com/msgrd?v=3&uin=$qq_number_3&site=qq&menu=yes'><img border='0' src='http://wpa.qq.com/pa?p=2:12345678:51' alt=$qq_number_3 title=$slogan></a></li>";
+//        echo "<li><a target='_blank' href='http://wpa.qq.com/msgrd?v=3&uin=$qq_number_4&site=qq&menu=yes'><img border='0' src='http://wpa.qq.com/pa?p=2:12345678:51' alt=$qq_number_4 title=$slogan></a></li>";
+        if (count($phones) > 0) {
+            foreach($phones as $key=>$value) {
+                echo "<li>电话：$value</li>";
+            }
+        }
         echo '</ul>';
         echo '</div>';
         echo '</div>';
@@ -120,6 +133,7 @@ class ZhulangOnlineSupport
     }
 
     public function topMenuHtml() {
+        $title          =  get_option($this->option_title);
         $qq_number_1    =  get_option($this->option_qq_number_1);
         $qq_number_2    =  get_option($this->option_qq_number_2);
         $qq_number_3    =  get_option($this->option_qq_number_3);
@@ -128,12 +142,13 @@ class ZhulangOnlineSupport
         $support_time   =  get_option($this->option_support_time);
         echo '<div class="wrap">';
         echo '<form name="option_form" method="post" action="">';
-        echo '<p style="font-weight:bold;">在此处对在线客服进行设置（4个QQ可以设置为同一个）。</p>';
-        echo "<p>联系QQ1：<input type='text' name='qq_number_1' value='$qq_number_1'></p>";
-        echo "<p>联系QQ2：<input type='text' name='qq_number_2' value='$qq_number_2'></p>";
-        echo "<p>联系QQ3：<input type='text' name='qq_number_3' value='$qq_number_3'></p>";
-        echo "<p>联系QQ4：<input type='text' name='qq_number_4' value='$qq_number_4'></p>";
-        echo "<p>联系电话：<input type='text' name='phone' value='$phone'></p>";
+        echo '<p style="font-weight:bold;">在此处对在线客服进行设置。</p>';
+        echo "<p>标题：<input type='text' name='title' value='$title'></p><br/>";
+        echo "<p>联系QQ<u style=\"color: red\">多个QQ使用逗号（英文逗号）“,”隔开。</u>：</p><textarea style='height:150px;width:500px' name='qq_number_1'>" . $qq_number_1 . '</textarea><br/>';
+//        echo "<p>联系QQ2：<input type='text' name='qq_number_2' value='$qq_number_2'></p>";
+//        echo "<p>联系QQ3：<input type='text' name='qq_number_3' value='$qq_number_3'></p>";
+//        echo "<p>联系QQ4：<input type='text' name='qq_number_4' value='$qq_number_4'></p>";
+        echo "<p>联系电话<u style=\"color: red\">多个电话使用逗号（英文逗号）“,”隔开。</u>：</p><textarea style='height:150px;width:500px' name='phone'>" . $phone . '</textarea>';
 //        echo "<p>在线时间：<input type='text' name='support_time' placeholder='示例  周一至周五：09:00-18:00' style='width: 230px' value='$support_time'></p>";
         if ($qq_number_1 || $qq_number_2 || $qq_number_3 || $qq_number_4 || $phone || $support_time) {
             echo '<div><p style="color:blue"><strong>设置已保存。</strong></p></div>';
@@ -146,18 +161,24 @@ class ZhulangOnlineSupport
 
     public function saveOptions() {
         // 保存设置页面的值
-        $qq_number_1    =  $_POST['qq_number_1'];
-        $qq_number_2    =  $_POST['qq_number_2'];
-        $qq_number_3    =  $_POST['qq_number_3'];
-        $qq_number_4    =  $_POST['qq_number_4'];
-        $phone          =  $_POST['phone'];
-        $support_time   =  $_POST['support_time'];
-        $qq_number_1 ? update_option($this->option_qq_number_1, $qq_number_1) : null;
-        $qq_number_2 ? update_option($this->option_qq_number_2, $qq_number_2) : null;
-        $qq_number_3 ? update_option($this->option_qq_number_3, $qq_number_3) : null;
-        $qq_number_4 ? update_option($this->option_qq_number_4, $qq_number_4) : null;
-        $phone ? update_option($this->option_phone, $phone) : null;
-        $support_time ? update_option($this->option_support_time, $support_time) : null;
+        if ($_POST) {
+            // TODO：未进行联系信息的正确性校验，正确手机号？正确QQ号？
+            $title          =  $_POST['title'];
+            $qq_number_1    =  $_POST['qq_number_1'];
+            if (!$qq_number_1) {
+                echo "<script>alert('请填写至少填写一个QQ号！')</script>";
+                return;
+            }
+            $phone          =  $_POST['phone'];
+            if (!$phone) {
+                echo "<script>alert('请填写至少填写一个联系电话！')</script>";
+                return;
+            }
+            $support_time   =  $_POST['support_time'];
+            $title ? update_option($this->option_title, $title) : null;
+            $qq_number_1 ? update_option($this->option_qq_number_1, $qq_number_1) : null;
+            $phone ? update_option($this->option_phone, $phone) : null;
+        }
     }
 }
 
--- zhulang-postmanage/zhulang-postmanage.php
+++ zhulang-postmanage/zhulang-postmanage.php
@@ -11,6 +11,9 @@ Author URI: https://github.com/LeoStupidWise/wp-plugins
 
 class ZhulangPostManage
 {
+    /*
+     * 1. 文章会有新的表来记录浏览信息，但总浏览量在 postmeta 中可以快速获取
+     * */
     public $name_cn                 =  '逐浪文章管理';
     public $name_en                 =  'zhulangPostManage';
     private $plugin_file_name      =  '';
@@ -24,11 +27,13 @@ class ZhulangPostManage
     private $menu_page_slug_top   =  '';
     private $table_name            =  'wp_zoe_post_click';                  // 文章浏览记录表
     private $view_meta_name        =  '_zoe_views';                         // 文章浏览量额外属性的键名
+    private $did_post_added        =  false;                                 // 一起请求一篇文章只记录一次浏览，当此属性为true时，禁止插入新的浏览记录
 
 
     public function __construct()
     {
         global $wpdb;
+        date_default_timezone_set('Asia/Shanghai');
         $this->plugin_file_name    =  dirname(plugin_basename(__FILE__));
         $this->full_url_prefix     =  home_url('wp-content/plugins/' . $this->plugin_file_name);
         $this->option_prefix       =  'zl_zoe_' . $this->name_en;
@@ -37,17 +42,50 @@ class ZhulangPostManage
     }
 
     public function doAction() {
-        global $post;
-        if (is_single($post->ID)) {
-            // 只有当前是文章的时候才进行文章浏览量的统计等等
-            if (!$this->haveTable($this->table_name)) {
-                $this->createTable();
+
+        if (!$this->haveTable($this->table_name)) {
+            $this->createTable();
+        }
+        add_action('the_post', [$this, 'addViewTime']);
+        add_action( 'widgets_init', [$this, 'registerWidget']);
+        // 使用 the_post 钩子会在页面加载文章的任何时候进行挂载，那么在 sidebar 里面的近期文章进行加载的时候，也会写入数据
+
+        add_action('pre_get_posts', [$this, 'doPostOrder']);
+        // 在输出文章列表之前，对文章进行排序
+    }
+
+    public function registerWidget() {
+        register_widget('ZhulangPostOrderWidget');
+    }
+
+    public function doPostOrder($query) {
+        //
+        if ($_POST) {
+            $post_order    =  $_POST['post_order'];
+
+            switch($post_order) {
+                default:
+                    break;
+                case 'order_time_desc':
+                    break;
+                case 'order_time_asc':
+                    $query->set( 'order', 'asc' );
+                    break;
+                case 'order_hot':
+                    /*
+                     * Excludes an array of single post ID's from your home page
+                        function exclude_single_posts_home($query) {
+                            if ($query->is_home() && $query->is_main_query()) {
+                                $query->set('post__not_in', array(7,11));
+                             }
+                        }
+
+                    add_action('pre_get_posts', 'exclude_single_posts_home');
+                     * */
+                    break;
+                case 'order_random':
+                    break;
             }
-            echo $post->ID;
-            var_dump($this->addPostViewsMeta($post->ID));
-            exit();
-        } else {
-            echo '我特么不是文章';
         }
     }
 
@@ -58,8 +96,10 @@ class ZhulangPostManage
         $sql    =  "DROP TABLE IF EXISTS `$this->table_name`;
             CREATE TABLE `$this->table_name` (
               `id` int(11) NOT NULL AUTO_INCREMENT,
-              `visit_ip` VARCHAR(16) DEFAULT NULL COMMENT '浏览者ip',
+              `post_id` int(11) DEFAULT NULL,
+              `visitor_ip` VARCHAR(16) DEFAULT NULL COMMENT '浏览者ip',
               `user_agent` VARCHAR(50) DEFAULT NULL COMMENT 'http_user_agent',
+              `url` VARCHAR(100) DEFAULT NULL COMMENT '用户评论的 URl',
               `created_at` datetime DEFAULT NULL COMMENT '创建时间',
               PRIMARY KEY (`id`)
             ) ENGINE=InnoDB DEFAULT CHARSET=utf8;";
@@ -69,7 +109,13 @@ class ZhulangPostManage
     public function addPostViewsMeta($post_id) {
         // 增加文章的浏览次数额外属性
         $views    =  get_post_meta($post_id, $this->view_meta_name, true);
-        return $views;
+        if (!$views) {
+            $result    =  add_post_meta($post_id, $this->view_meta_name, 1, true);
+            return 1;
+        } else {
+            $result    =  update_post_meta($post_id, $this->view_meta_name, $views+1);
+            return $views+1;
+        }
     }
 
     public function haveTable($table_name) {
@@ -81,7 +127,144 @@ class ZhulangPostManage
 
     public function addViewTime() {
         // 增加浏览次数一次
+        global $post;
+        if( is_int( $post ) ) {
+            $post = get_post( $post );
+        }
+        if( ! wp_is_post_revision( $post ) && ! is_preview() ) {
+            if (is_single() || is_page()) {
+                // 到此进入文章页
+                if (!$this->did_post_added) {
+                    // 页面中还有侧边栏，侧边栏中的文章加载时会更新 global $post，但一般情况下侧边栏会后加载
+                    // 所以设置一个状态，只要页面写入过一次数据库就将这个状态变为 true
+                    // 只有当状态为 false 时才能写入数据
+                    // SW.Leo 2017-8-22
+                    $post_id       =  $post->ID;
+                    $user_agent    =  $_SERVER['HTTP_USER_AGENT'];
+                    $client_ip     =  $this->getClientIP();
+                    $url           =  home_url();
+                    $time          =  date('Y-m-d H:i:s');
+                    $result        =  $this->dbAddViewTime($post_id, $client_ip, $url, $user_agent, $time);
+                    $views         =  $this->addPostViewsMeta($post_id);
+                    if ($result) {
+                        $this->did_post_added    =  true;
+                    }
+                }
+            }
+        }
+    }
 
+    public function getVisitorInfo() {
+        // 获取访问者的 ip，user_agent，url 等等
+        $user_agent    =  $_SERVER['user_agent'];
+        $client_ip     =  $this->getClientIP();
+        $url           =  home_url();
+    }
+
+    public function dbAddViewTime($post_id, $visit_ip, $url, $user_agent, $time=null) {
+        if (!$time) {
+            $time      =  date('Y-m-d H:i:s');
+        }
+        $sql    =  "INSERT INTO $this->table_name (post_id, visitor_ip, user_agent, url, created_at) VALUES ($post_id, '$visit_ip', '$user_agent', '$url', '$time')";
+        return $this->wp_db->query($sql);
+    }
+
+    public function getClientIP()
+    {
+        if (getenv("HTTP_CLIENT_IP"))
+            $ip = getenv("HTTP_CLIENT_IP");
+        else if (getenv("HTTP_X_FORWARDED_FOR"))
+            $ip = getenv("HTTP_X_FORWARDED_FOR");
+        else if (getenv("REMOTE_ADDR"))
+            $ip = getenv("REMOTE_ADDR");
+        else $ip = "Unknow";
+        return $ip;
+    }
+}
+
+class ZhulangPostOrderWidget extends WP_Widget
+{
+    public function __construct()
+    {
+        $id_base    =  'zhulangpostordertool';
+        /*Optional Base ID for the widget, lowercase and unique. If left empty,a portion of the widget's class name will be used Has to be unique.*/
+
+        $name       =  '逐浪文章排序小工具';
+        /*Name for the widget displayed on the configuration page.*/
+
+        $widget_options    =  [
+            'classname'    => 'ZhulangPostOrderWidget',
+            'description'  => '浏览量排序，热度排序，随机排序。'
+        ];
+
+        $control_options   =  'widgets_control';
+        parent::__construct($id_base, $name, $widget_options, $control_options);
+    }
+
+    public function form($instance) {
+        $defaults = [
+            'title'         => '文章排序',
+            'order_time'   => '时间排序',
+            'order_hot'     => '热度排序',
+            'order_random'  => '随机排序'
+        ];
+        $instance = wp_parse_args( (array) $instance, $defaults );
+
+        $title         = $instance['title'];
+        $order_time    = $instance['order_time'];
+        $order_hot     = $instance['order_hot'];
+        $order_random  = $instance['order_random'];
+        ?>
+        <p>标题: <input class="widefat" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo esc_attr( $title ); ?>" /></p>
+        <p>排序一: <input class="widefat" name="<?php echo $this->get_field_name( 'order_time' ); ?> "type="text" value="<?php echo esc_attr( $order_time ); ?> " /></p>
+        <p>排序二: <input class="widefat" name="<?php echo $this->get_field_name( 'order_hot' ); ?> "type="text" value="<?php echo esc_attr( $order_hot ); ?> " /></p>
+        <p>排序三: <input class="widefat" name="<?php echo $this->get_field_name( 'order_random' ); ?> "type="text" value="<?php echo esc_attr( $order_random ); ?> " /></p>
+        <?php
+//        return parent::form($instance); // TODO: Change the autogenerated stub
+    }
+
+    public function update($new_instance, $old_instance) {
+        $instance = $old_instance;
+
+        $instance['title'] = strip_tags( trim( $new_instance['title'] ) );
+        $instance['order_time'] = strip_tags( trim(  $new_instance['order_time'] ) );
+        $instance['order_hot'] = strip_tags( trim( $new_instance['order_hot'] ) );
+        $instance['order_random'] = strip_tags( trim( $new_instance['order_random'] ) );
+//        return parent::update($new_instance, $old_instance); // TODO: Change the autogenerated stub
+    }
+
+    public function widget($args, $instance) {
+        ?>
+
+        <div>
+            <form method="post" action="#">
+                <select name="post_order" style="width: 150px; height: 30px">
+                    <option value ="order_time_desc">时间降序</option>
+                    <option value ="order_time_asc">时间升序</option>
+                    <option value ="order_hot">热度排序</option>
+                    <option value="order_random">随机排序</option>
+                </select>
+                <input style="height: auto; width: auto;" type="submit">
+            </form>
+        </div>
+
+        <?php
+
+        /*
+         * 暂时无法实现通过后台来控制前台的显示内容，这里直接写死
+         * SW.Leo 2017-8-22
+         * */
+/*        extract( $args );
+        $title = apply_filters( 'widget_title', $instance['title'] );
+        $order_time    = empty( $instance['order_time'] ) ? ' ' : $instance['order_time'];
+        $order_hot     = empty( $instance['order_hot'] ) ? ' ' : $instance['order_hot'];
+        $order_random  = empty( $instance['order_random'] ) ? ' ' : $instance['order_random'];
+
+        echo '<p> 标题: ' . $title . '</p>';
+        echo '<p> 排序1: ' . $order_time. '</p>';
+        echo '<p> 排序2: ' . $order_hot. '</p>';
+        echo '<p> 排序3: ' . $order_random. '</p>';*/
+//        parent::widget($args, $instance); // TODO: Change the autogenerated stub
     }
 }
 
